15c94cd7cd26fddd4fe8d03cb3f366b967886cb1
ndaDayo <hayato711+nda@gmail.com>
2023-08-26 12:33:59 +0900

    Initial Commit

---
+++ ArchSandbox/ArchSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchSandboxApp.swift
+//  ArchSandbox
+//
+//  Created by hayato sugawara on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ArchSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
